6fce25830fa340761946bf034b8971952e87bb2e
tomL1985 <tom.lennartz@gmail.com>
2024-04-28 18:12:54 +0200

    adding routing to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter as Router, Route, Routes, useNavigate} from 'react-router-dom';
+import NotFound from "./pages/NotFound/NotFound.jsx";
+import HomePage from './pages/HomePage/HomePage.jsx';
+import PhotoDetailPage from './pages/PhotoDetailPage';
+import TopicPhotos from "./pages/TopicPhotos/TopicPhotos.jsx";
+import MyPins from "./pages/MyPins/MyPins.jsx";
+import Contact from "./pages/Contact/Contact.jsx";
+import About from "./pages/About/About.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <AuthContextProvider>
+            <PinnedPhotosProvider>
+                <Router>
+                    <NavBar />
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/photo/:id" element={<PhotoDetailPage />} />
+                        <Route path="/topic/:topicId" element={<TopicPhotos />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/mypins" element={<MyPins />} />
+                        <Route path="/registration" element={<Registration />} />
+                        <Route path="/admin" element={<AdminRoute />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Router>
+            </PinnedPhotosProvider>
+        </AuthContextProvider>
+    );
 }
 
 export default App
