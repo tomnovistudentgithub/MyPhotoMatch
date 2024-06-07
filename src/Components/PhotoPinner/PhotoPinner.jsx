1dca0962620a184ccb906f2be6998e380bda604d
tomnovistudentgithub <tom.lennartz@gmail.com>
2024-06-07 15:55:10 +0200

    useCallback in photoPinner to only render when needed with log

--- src/Components/PhotoPinner/PhotoPinner.jsx
+++ src/Components/PhotoPinner/PhotoPinner.jsx
@@ -24,6 +24,7 @@ function PhotoPinner({ photo, isTopicPage }) {
 
     const handleUnpinPhoto = useCallback(() => {
         togglePinPhoto(photo);
+        console.log('unpinning photo');
     }, [photo, togglePinPhoto]);
 
     useEffect(() => {
