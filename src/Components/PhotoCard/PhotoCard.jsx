53e7060ff96fd9056c17e310f1b08870593c0a4e
tomnovistudentgithub <tom.lennartz@gmail.com>
2024-06-16 13:49:27 +0200

    using the photocard component behind the contactform so that it is a reused component

--- src/Components/PhotoCard/PhotoCard.jsx
+++ src/Components/PhotoCard/PhotoCard.jsx
@@ -2,23 +2,26 @@ import React, {useContext} from 'react';
 import styles from './PhotoCard.module.css';
 import PhotoPinner from "../PhotoPinner/PhotoPinner.jsx";
 import PinnedPhotosContext from "../../contexts/PinnedPhotoContext.js";
+import classNames from 'classnames';
 
-function PhotoCard({ photo }) {
+function PhotoCard({ photo, className, hidePinButton }) {
 
     const {error, isTopicPage} = useContext(PinnedPhotosContext);
 
-
     return (
-        <div className={styles['photo-card-inner-container']}>
+        <div className={classNames(styles['photo-card-inner-container'], {[styles[className]]: className})}>
             <div className={styles['photo-card-image-container']}>
-                <img className={styles['photo-card-image']} src={photo.urls.small} alt={photo.alt_description}/>
-                <PhotoPinner photo={photo} isTopicPage={isTopicPage}/>
+                <img className={styles['photo-card-image']}
+                     src={photo.urls && photo.urls.small ? photo.urls.small : photo.url} alt={photo.alt_description}/>
+                {!hidePinButton && <PhotoPinner photo={photo} isTopicPage={isTopicPage}/>}
             </div>
-            <div className={styles['photo-card-info']}>
-                <p>{photo.user && "Photo from Unsplashed user: "} </p>
-                <b> {photo.user.username}</b>
-                {error && <p>{error}</p>}
-            </div>
+            {className !== "photo-card-contact" && (
+                <div className={styles['photo-card-info']}>
+                    <p>{photo.user && "Photo from Unsplashed user: "} </p>
+                    <b> {photo.user ? photo.user.username : photo.username}</b>
+                    {error && <p>{error}</p>}
+                </div>
+            )}
         </div>
     );
 }
